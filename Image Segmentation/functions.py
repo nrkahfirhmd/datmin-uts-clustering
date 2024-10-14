c63ba9913f4f84a825ee321a80019812a2663edb
Nurkahfi A Rahmada <110613182+nrkahfirhmd@users.noreply.github.com>
2024-10-14 14:24:41 +0700

    image repaint function done

--- Image Segmentation/functions.py
+++ Image Segmentation/functions.py
@@ -4,30 +4,32 @@ import pandas as pd
 from skimage.transform import resize
 from imageio.v2 import imread
 from kmeans import kmeans
-from sklearn.cluster import KMeans
 from matplotlib.colors import to_hex
 from PIL import Image
 
 def predict_color(file, k):
     image = imread(file)
+
+    h, w = image.shape[:2]
+    out_h = 500
+
+    image = resize(image, (out_h, int(out_h*float(w)/h)), anti_aliasing=True)
     
     st.image(image, caption="Input Image")
-    
-    image = resize(image, (250, 250), anti_aliasing=True)
-    
+
     if image.shape[-1] != 3:
         st.error("Image does not have expected RGB channels.")
         return
-    
-    data = image.reshape(-1, 3)
-    
+
+    data = np.array(image.reshape(-1, 3))
+
     clusters, pallete = kmeans(data, k=k)
-    
+
     pallete_list = list()
-    
+
     for color in pallete:
         pallete_list.append(tuple(color))
-    
+
     for color in pallete_list:
         color_img = np.zeros((100, 100, 3), dtype=np.uint8)
         color_img[:, :] = (np.array(color) * 255).astype(np.uint8)
@@ -36,3 +38,15 @@ def predict_color(file, k):
 
         st.image(color_pil_img, width=100)
         st.write(to_hex(color))
+
+    repaint(data, clusters, pallete_list)
+
+def repaint(image, clusters, pallete, out_h=500):
+    pic = image.copy()
+
+    for px in range(image.shape[0]):
+        pic[px] = pallete[clusters[px]]
+
+    pic = pic.reshape(out_h, -1, 3)
+
+    st.image(pic, caption="Output Image")
