81ed2a043aa56a436f720e8fe4e16828a5948000
Nurkahfi A Rahmada <110613182+nrkahfirhmd@users.noreply.github.com>
2024-10-16 10:47:32 +0700

    adding color distributions also reworking the ui

--- Image Segmentation/app.py
+++ Image Segmentation/app.py
@@ -5,7 +5,7 @@ st.title("Image Segmentation Clustering")
 
 file = st.file_uploader("Upload a JPEG, JPG, or PNG Image", ['.JPEG', ".JPG", ".PNG"])
 k = st.number_input("Numbers of Clusters", min_value=2, max_value=10, value=5)
-predict = st.button("Predict")
+predict = st.button("Cluster")
 
 if predict and file is not None:
     f.predict_color(file, k)
--- Image Segmentation/functions.py
+++ Image Segmentation/functions.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from skimage.transform import resize
 from imageio.v2 import imread
 from kmeans import kmeans
@@ -15,9 +16,9 @@ def predict_color(file, k):
     h, w = image.shape[:2]
     out_h = 250
 
-    image = resize(image, (out_h, int(out_h*float(w)/h)))
-    
     st.image(image, caption="Input Image", width=750)
+
+    image = resize(image, (out_h, int(out_h*float(w)/h)), anti_aliasing=True)
 
     if image.shape[-1] != 3:
         st.error("Image does not have expected RGB channels.")
@@ -26,21 +27,21 @@ def predict_color(file, k):
     data = np.array(image.reshape(-1, 3))
 
     clusters, pallete = kmeans(data, k=k)
-
-    pallete_list = [tuple(color) for color in pallete]
-
-    for color in pallete_list:
-        color_img = np.zeros((100, 100, 3), dtype=np.uint8)
-        color_img[:, :] = (np.array(color) * 255).astype(np.uint8)
-
-        color_pil_img = Image.fromarray(color_img)
-
-        st.image(color_pil_img, width=100)
-        st.write(to_hex(color))
-
-    repaint(data, clusters, pallete_list, out_h)
     
+    pallete_list = [tuple(color) for color in pallete]
+    
+    st.header("Feature Extractions")
+
+    st.subheader("Dominant Color")
+    get_dominant_colors(pallete_list, k)
+    
+    st.subheader("Color Distributions")
+    get_color_distribution(clusters, data.shape[0], pallete_list, k)
+
+    st.header("Clustering Result")
     metrics(data, clusters)
+    
+    repaint(data, clusters, pallete_list, out_h)
 
 def repaint(image, clusters, pallete, out_h=500):
     pic = image.copy()
@@ -56,5 +57,38 @@ def metrics(data, label):
     silhouette = silhouette_score(data, label)
     davies_bouldin = davies_bouldin_score(data, label)
     
-    st.text(f"Silhouette Coefficient : {round(silhouette, 4)}")
-    st.text(f"Davies-Bouldin Index : {round(davies_bouldin, 4)}")
+    metrics1, metrics2 = st.columns(2)
+    
+    metrics1.metric("Silhouette Coefficient", round(silhouette, 4))
+    metrics2.metric("Davies-Bouldin Index", round(davies_bouldin, 4))
+    
+def get_dominant_colors(pallete_list, k):
+    cols = st.columns(k)
+    
+    for i, color in enumerate(pallete_list):
+        color_img = np.zeros((100, 100, 3), dtype=np.uint8)
+        color_img[:, :] = (np.array(color) * 255).astype(np.uint8)
+
+        color_pil_img = Image.fromarray(color_img)
+
+        with cols[i]:
+            st.image(color_pil_img, width=100)
+            st.write(to_hex(color))
+            
+def get_color_distribution(clusters, pixels, colors, k):
+    percentage = np.asarray(np.unique(clusters, return_counts=True)[1], dtype='float32')
+    percentage = percentage / pixels
+    
+    dominance = [[percentage[px], colors[px]] for px in range(k)]
+    dominance = sorted(dominance, key=lambda x: x[0], reverse=True)
+    
+    patch = np.zeros((50, 750, 3), dtype=np.uint8)
+    
+    start = 0
+    for cx in range(k):
+        width = int(dominance[cx][0] * patch.shape[1])
+        end = start + width
+        patch[:, start:end, :] = (np.array(dominance[cx][1]) * 255).astype(np.uint8)
+        start = end
+    
+    st.image(Image.fromarray(patch))
